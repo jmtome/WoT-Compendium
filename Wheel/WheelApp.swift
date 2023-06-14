9c99c47c04a84705a7eec46b1a6e8a1e96204ac4
jmtome <jtome@fi.uba.ar>
2023-06-13 22:24:27 -0300

    Initial Commit

---
+++ Wheel/WheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelApp.swift
+//  Wheel
+//
+//  Created by macbook on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
